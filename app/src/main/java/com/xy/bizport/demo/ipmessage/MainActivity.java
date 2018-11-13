60a06676398b1e367d1e9bd264b24e8f7aafd71d
pengyanjun <pengyanjun@mfexcel.com>
2018-11-13 16:17:34 +0800

    QQ分享报错：[SQ10004]QQ图片存储失败，请检查图片或者检查是否有读写权限。增加6.0以上系统访问文件权限申请

--- app/src/main/java/com/xy/bizport/demo/ipmessage/MainActivity.java
+++ app/src/main/java/com/xy/bizport/demo/ipmessage/MainActivity.java
@@ -1,6 +1,9 @@
 package com.xy.bizport.demo.ipmessage;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +11,14 @@ import android.widget.Toast;
 
 import com.xy.bizport.share.ShareTool;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
 
+@RuntimePermissions
 public class MainActivity extends Activity implements View.OnClickListener{
 
     public static final int SHARE_REQUEST_CODE = 1001;
@@ -17,8 +27,53 @@ public class MainActivity extends Activity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        MainActivityPermissionsDispatcher.showSTORAGEWithPermissionCheck(this);
+        initView();
+    }
+    void initView(){
         findViewById(R.id.pop_btn).setOnClickListener(this);
         findViewById(R.id.share_activity_btn).setOnClickListener(this);
+    }
+
+    @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showSTORAGE(){
+        Toast.makeText(this, "访问文件权限已获取", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnShowRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showRationaleForSTORAGE(final PermissionRequest request){
+        new AlertDialog.Builder(this)
+                .setMessage("申请访问文件权限")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.proceed();//继续执行请求
+                    }
+                })
+                .setNegativeButton("", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.cancel();//取消执行请求
+                    }
+                })
+                .show();
+    }
+
+    @OnPermissionDenied(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showDeniedForSTORAGE() {
+        Toast.makeText(this, "访问文件权限被拒绝", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showNeverAskForSTORAGE() {
+        Toast.makeText(this, "已拒绝访问文件权限，并不再询问", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
     }
 
     @Override
@@ -28,24 +83,28 @@ public class MainActivity extends Activity implements View.OnClickListener{
                 startActivity(new Intent(this,SharePopWindowActivity.class));
                 break;
             case R.id.share_activity_btn:
-                try{
-                    //在当前页面打开分享透明activity
-                    Bundle bundle = new Bundle();
-                    bundle.putString(ShareTool.SHARE_TITLE, "title-特稿｜陆家嘴金融骗子和P2P“爆雷潮”，全是自作孽不可活！");
-                    bundle.putString(ShareTool.SHARE_CONTENT, "content-位于上海浦东新区黄浦江畔的陆家嘴金融中心，是众多跨国银行的大中华区或东亚总部所在地，是中国最具影响力的金融中心之一。");
-                    bundle.putString(ShareTool.SHARE_URL, "http://gz.feixin.10086.cn/I8LG36_eu2QVr");
-                    bundle.putString(ShareTool.SHARE_IMAGE_URL, "http://pa.rcscdn.fetionpic.com//Public/Uploads/user/4/7/62/2/497834762/imgs/5b6015a52540d.jpg");
-                    Intent intent = new Intent("com.xy.bizport.share.ACTION_SHARE");
-                    intent.putExtras(bundle);
-                    startActivity(intent);
-//                startActivityForResult(intent, SHARE_REQUEST_CODE);
-
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
+                share();
                 break;
         }
 
+    }
+
+    void share(){
+        try{
+            //在当前页面打开分享透明activity
+            Bundle bundle = new Bundle();
+            bundle.putString(ShareTool.SHARE_TITLE, "title-特稿｜陆家嘴金融骗子和P2P“爆雷潮”，全是自作孽不可活！");
+            bundle.putString(ShareTool.SHARE_CONTENT, "content-位于上海浦东新区黄浦江畔的陆家嘴金融中心，是众多跨国银行的大中华区或东亚总部所在地，是中国最具影响力的金融中心之一。");
+            bundle.putString(ShareTool.SHARE_URL, "http://gz.feixin.10086.cn/I8LG36_eu2QVr");
+            bundle.putString(ShareTool.SHARE_IMAGE_URL, "http://pa.rcscdn.fetionpic.com//Public/Uploads/user/4/7/62/2/497834762/imgs/5b6015a52540d.jpg");
+            Intent intent = new Intent("com.xy.bizport.share.ACTION_SHARE");
+            intent.putExtras(bundle);
+            startActivity(intent);
+//                startActivityForResult(intent, SHARE_REQUEST_CODE);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
